c04a17ec881361e5fae2b624f244ac35bbf8991d
Gabriel Araujo <gabrielaraujo2014.com@outlook.com>
2026-02-25 22:35:17 -0300

    hello world

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import "dotenv/config"
+import Fastify from 'fastify'
+import {serializerCompiler, validatorCompiler, ZodTypeProvider} from 'fastify-type-provider-zod';
+import {z} from "zod";
+const app = Fastify({
+    logger: true
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        description: "hello world",
+        tags:["Hello world"],
+        response: {
+            200: z.object({
+                message: z.string(),
+            }),
+        },
+    },
+    handler: () => {
+        return {
+            message: "Hello world",
+        }
+    },
+});
+
+
+try {
+    await app.listen({ port: Number(process.env.PORT) ?? 8081 })
+} catch (err) {
+    app.log.error(err)
+    process.exit(1)
+}
