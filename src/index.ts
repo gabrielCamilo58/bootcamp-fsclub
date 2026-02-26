8b158b187d12e935a13ce9bf4cf90dd40831a360
Gabriel Araujo <gabrielaraujo2014.com@outlook.com>
2026-02-26 20:19:46 -0300

    configuracoes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config"
+import {fastifySwagger} from "@fastify/swagger";
+import {fastifySwaggerUi} from "@fastify/swagger-ui";
 import Fastify from 'fastify'
-import {serializerCompiler, validatorCompiler, ZodTypeProvider} from 'fastify-type-provider-zod';
+import {jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider} from 'fastify-type-provider-zod';
 import {z} from "zod";
 const app = Fastify({
     logger: true
@@ -8,7 +10,23 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
-
+await app.register(fastifySwagger,{
+    openapi:{
+        info:{
+            title: "API Documentation API",
+            description: "API Documentation API documentation API",
+            version: "1.0.0",
+        },
+        servers:[{
+            description: "localhost",
+            url: "http://localhost:8081",
+        }],
+    },
+    transform:jsonSchemaTransform,
+});
+await app.register(fastifySwaggerUi,{
+    routePrefix:'/docs',
+})
 app.withTypeProvider<ZodTypeProvider>().route({
     method: 'GET',
     url: '/',
