71abf64b355cfccbd82a1d1a77150eacd40498c1
Gabriel Araujo <gabrielaraujo2014.com@outlook.com>
2026-02-26 20:46:28 -0300

    better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import {PrismaClient} from "../generated/prisma/client.js";
+import {PrismaPg} from "@prisma/adapter-pg";
+import {openAPI} from "better-auth/plugins";
+import {betterAuth} from "better-auth";
+import {prismaAdapter} from "better-auth/adapters/prisma";
+
+
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({connectionString: process.env.DATABASE_URL}),
+})
+export const auth = betterAuth({
+    trustedOrigins: ["http://localhost:3000"],
+    emailAndPassword:{
+        enabled: true,
+    },
+    database: prismaAdapter(prisma,{
+        provider: "postgresql",
+    }),
+    plugins:[openAPI()],
+})
